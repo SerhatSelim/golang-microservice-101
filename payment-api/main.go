d1ec539c4d5e6df3ae940fb0b71b7a4b69e6f491
Serhat Selim <serhatselim@hotmail.com.tr>
2022-10-24 21:21:52 +0300

    payment message

--- payment-api/main.go
+++ payment-api/main.go
@@ -4,17 +4,27 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type PaymentMessage struct {
+	Amount int    `form:"Amount" query:"Amount"`
+	From   string `form:"from" query:"from"`
+	To     string `form:"to" query:"to"`
+}
+
 func main() {
 
 	app := fiber.New()
 
+	//curl localhost:54321/api/v1/healty
 	app.Get("/api/v1/healty", func(c *fiber.Ctx) error {
 		return c.SendString("payment-api healty")
 	})
 
+	//curl -X POST -H "Content-Type: application/json"  -d '{"Amount": 100,"From": "cobadeff","To": "nabatww"}' localhost:54321/api/v1/payment
 	app.Post("/api/v1/payment", func(c *fiber.Ctx) error {
-		c.Method() // "POST"
-		return c.SendString("payment-api payment")
+		var dto PaymentMessage
+		c.BodyParser(&dto)
+		c.SendString("payment-api payment")
+		return c.JSON(dto)
 
 	})
 
