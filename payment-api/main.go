8fa1d20e151e2eff53ef29da3704564412f0018b
Serhat Selim <serhatselim@hotmail.com.tr>
2022-10-24 20:56:18 +0300

    post

--- payment-api/main.go
+++ payment-api/main.go
@@ -8,8 +8,14 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/healty", func(c *fiber.Ctx) error {
-		return c.SendString("payment-api running")
+	app.Get("/api/v1/healty", func(c *fiber.Ctx) error {
+		return c.SendString("payment-api healty")
+	})
+
+	app.Post("/api/v1/payment", func(c *fiber.Ctx) error {
+		c.Method() // "POST"
+		return c.SendString("payment-api payment")
+
 	})
 
 	app.Listen(":54321")
