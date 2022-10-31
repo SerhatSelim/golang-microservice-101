48ddd7a912b08dd7cf8c819db9683a1e3586266a
Serhat Selim <serhatselim@hotmail.com.tr>
2022-10-31 22:02:35 +0300

    fastpay post
    send fastpay request

--- payment-gateway/main.go
+++ payment-gateway/main.go
@@ -17,6 +17,13 @@ type PaymentMessage struct {
 	To     string `form:"to" json:"to"`
 }
 
+type FastPayMessage struct {
+	Amount int    `form:"amount" json:"amount"`
+	From   string `form:"from" json:"from"`
+	To     string `form:"to" json:"to"`
+	Flag   bool   `form:"flag" json:"flag"`
+}
+
 func main() {
 
 	app := fiber.New()
@@ -27,7 +34,7 @@ func main() {
 
 	})
 
-	// //curl localhost:30001/api/v1/gateway/payment-api/healty
+	//curl localhost:30001/api/v1/gateway/payment-api/healty
 	app.Get("/api/v1/gateway/payment-api/healty", func(c *fiber.Ctx) error {
 		fmt.Println("/api/v1/gateway/payment-api/healty")
 		client := &http.Client{}
@@ -91,7 +98,7 @@ func main() {
 		return c.SendString(string(responseBody))
 	})
 
-	// //curl localhost:30001/api/v1/gateway/fastpay-api/healty
+	//curl localhost:30001/api/v1/gateway/fastpay-api/healty
 	app.Get("/api/v1/gateway/fastpay-api/healty", func(c *fiber.Ctx) error {
 		fmt.Println("/api/v1/gateway/fastpay-api/healty")
 		client := &http.Client{}
@@ -115,6 +122,43 @@ func main() {
 		fmt.Println(resp.Status)
 		fmt.Println(string(responseBody))
 
+		return c.SendString(string(responseBody))
+	})
+
+	//curl -X POST -H "Content-Type: application/json"  -d '{"amount": 100,"from": "cobadeff","to": "nabatww","flag": true}' localhost:30001/api/v1/gateway/fastpay
+	app.Post("/api/v1/gateway/fastpay", func(c *fiber.Ctx) error {
+		fmt.Println("api/v1/gateway/fastpay")
+
+		var dto FastPayMessage
+		c.BodyParser(&dto)
+		fastPayMessage := &dto
+		jsonData, err := json.Marshal(fastPayMessage)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(jsonData))
+
+		req, err := http.NewRequest(http.MethodPost, "http://localhost:63321/api/v1/fastpay", bytes.NewBuffer(jsonData))
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Errored when sending request to the server")
+			return err
+		}
+
+		defer resp.Body.Close()
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(resp.Status)
+		fmt.Println(string(responseBody))
 		return c.SendString(string(responseBody))
 	})
 
