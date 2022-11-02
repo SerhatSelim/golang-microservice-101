393e04a1d4fb09c29d5b743519b5d65cc052b34f
Serhat Selim <serhatselim@hotmail.com.tr>
2022-11-02 21:35:52 +0300

    reverse proxy
    moneygram gateway

---
+++ reverse-proxy/proxy.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Proxy interface {
+	Accept(key string) bool
+	Proxy(c *fiber.Ctx) error
+}
+
+var Proxies = []Proxy{
+	MoneygramProxy("moneygram"),
+}
+
+func MoneygramProxyHandler(c *fiber.Ctx) error {
+	for _, v := range Proxies {
+		if v.Accept(c.Params("key")) {
+			return v.Proxy(c)
+		}
+	}
+
+	c.Response().SetStatusCode(404)
+	return nil
+}
